f4ff6cfc0b546494404acb15937b6bbd02b5a31c
Edward Vaudin <edward@vaudin.net>
2022-11-24 22:02:56 +0000

    Created KebabContext
    Awaiting JSON configuration connection string.

---
+++ KebPOS/DataAccess/KebabContext.cs
@@ -0,0 +1,16 @@
+﻿using KebPOS.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace KebPOS.DataAccess;
+
+internal class KebabContext : DbContext
+{
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        // Data source to be replaced with config connection string.
+        optionsBuilder.UseSqlite("Data Source=KebabDb;");
+    }
+}

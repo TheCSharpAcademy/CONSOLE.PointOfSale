33f3bca27694eb22da4547baa2ac655b11ae0442
Spidermonk4n3 <spidermonk4n3@gmail.com>
2022-11-24 17:18:24 -0500

    added IsValidIdInput method

---
+++ KebPOS/Validation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KebPOS
+{
+    internal class Validation
+    {
+        static public bool IsValidIdInput(string input)
+        {
+
+            if (!int.TryParse(input, out int parsedInput))
+            {
+                return false;
+            }
+
+            return parsedInput > 0;
+        }
+    }
+}

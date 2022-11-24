46e57409db6f316a42d3c71062db09e7369e0a3a
zenraikou <zenraikou@gmail.com>
2022-11-24 22:10:15 +0800

    added product service

---
+++ KebPOS/Services/ProductService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KebPOS.Services
+{
+    internal class ProductService
+    {
+        public List<string> GetProducts()
+        {
+            var order = new List<string>();
+            order.AddRange(new string[] {
+                "Yogurt Kebab", "Shish Kebab", "Doner Kebab", "Kathi Kebab", "Chapli Kebab",
+                "Burrah Kebab", "Beyti Kebab", "Adana Kebab", "Chicken Sis Kebab", "Turkish Sis Kebab",
+                "Water", "Tea", "Milk", "Coffee", "Beer"
+            });
+            return order;
+        }
+    }
+}

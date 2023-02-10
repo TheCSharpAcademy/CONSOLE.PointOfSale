602b1db4332e1fd0d2cad300d7b9913672a25385
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-02-10 16:10:35 +0300

    Add method to validate if answer is yes, no, y, n or some other weird input

--- KebPOS/Validation.cs
+++ KebPOS/Validation.cs
@@ -6,4 +6,11 @@ public class Validation
     {
         return int.TryParse(input, out int parsedInput) ? parsedInput > 0 : false;
     }
+
+    public static bool IsValidAnswer(string? answer)
+    {
+        var answersArray = new string[4] { "y", "yes", "n", "no" };
+
+        return !string.IsNullOrWhiteSpace(answer) && answersArray.Contains(answer);
+    }
 }

a41c6985147cc9e8d95e36ab5eaae3c784c99dc3
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-02-10 16:11:01 +0300

    Add method to write orders to db

--- KebPOS/KebabController.cs
+++ KebPOS/KebabController.cs
@@ -16,4 +16,14 @@ public class KebabController
         using var db = new KebabContext();
         return db.Orders.OrderBy(x => x.OrderDate).ToList();
     }
+
+    public void AddOrders(Order order/*, List<OrderProduct> orderProductsList*/)
+    {
+        using var db = new KebabContext();
+
+        // db.OrderProducts.AddRange(orderProductsList);
+        db.Orders.Add(order);
+
+        db.SaveChanges();
+    }
 }
